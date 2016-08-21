b85a5fe921ac5ce6a07d1aeec711e524d1396ba9
Alexey Nikitin <nikitin@bookmate.com>
2016-08-21 20:18:50 +0300

    Mimic stdlib mutex

--- lib/probable_giggle/lock.rb
+++ lib/probable_giggle/lock.rb
@@ -8,33 +8,86 @@ module ProbableGiggle
       "#{MAX_NAME_LENGTH} characters".freeze
     DEFAULT_TIMEOUT = 0
 
-    attr_reader :name, :timeout
+    attr_reader :name
     attr_reader :logger, :connection
 
-    def initialize(name:, timeout: DEFAULT_TIMEOUT, logger: Configuration.logger, connection: Configuration.connection)
+    def initialize(name:, logger: Configuration.logger, connection: Configuration.connection)
       fail ArgumentError.new(MAX_NAME_LENGTH_ERROR_MSG) if name.length >= MAX_NAME_LENGTH
       @name = name
-      @timeout = timeout
       @logger = logger
       @connection = connection
     end
 
-    def obtain
+    def lock
+      # MariaDB doesn't support timeout: -1 which means wait infinite
+      # so emulate this behaviour
+      until obtain(10)
+        # try again
+      end
+      self
+    end
+
+    def locked?
+      !is_used_lock.nil?
+    end
+
+    def owned?
+      obtained_connection == self_connection
+    end
+
+    def sleep(timeout)
+      unlock
+      sleep timeout
+      try_lock
+    end
+
+    def synchronize
+      lock
+      yield
+    ensure
+      release
+    end
+
+    def try_lock
+      obtain(0)
+    end
+
+    def unlock
+      fail "Resource [#{name}] wasn't locked by the current connection" unless release
+    end
+
+    def get_lock(timeout = DEFAULT_TIMEOUT)
       logger.debug("Obtain lock for resource [#{name}]")
       exec("GET_LOCK(#{quoted_name}, #{timeout})")
     end
+    alias_method :obtain, :get_lock
 
-    def release
+    def release_lock
       logger.debug("Obtain lock for resource [#{name}]")
       exec("RELEASE_LOCK(#{quoted_name})")
     end
+    alias_method :release, :release_lock
+
+    def is_used_lock
+      select("IS_USED_LOCK(#{quoted_name})")
+    end
+    alias_method :obtained_connection, :is_used_lock
 
     private
 
-    def exec(fun)
+    def self_connection
+      select('CONNECTION_ID()'.freeze)
+    end
+
+    def select(fun)
       query = "SELECT #{fun} AS #{unique_column_name}"
-      result = connection.select_value(query)
-      logger.debug("Query: [#{query}]; Result: [#{result}]")
+      connection.select_value(query).tap do |result|
+        logger.debug("Query: [#{query}]; Result: [#{result}]")
+      end
+    end
+
+    def exec(fun)
+      result = select(fun)
       1 == result
     end
 
--- test/probable_giggle/lock_test.rb
+++ test/probable_giggle/lock_test.rb
@@ -3,17 +3,39 @@ require 'probable_giggle/lock'
 require 'support/fake_mysql_connection'
 
 class ProbableGiggle::LockTest < Minitest::Test
+  attr_reader :lock, :logger, :connection
+
   def setup
-    connection = FakeMysqlConnection.new
-    logger = Logger.new('/dev/null')
+    @connection = FakeMysqlConnection.new
+    @logger = Logger.new('/dev/null')
     @lock = ProbableGiggle::Lock.new(name: 'lock1', connection: connection, logger: logger)
   end
 
+  def test_lock
+    assert_equal(lock, lock.lock, 'Lock#lock grabs the lock and returns himself')
+  end
+
+  def test_locked_when_locked
+    connection = FakeMysqlConnection.new(return_value: 68)
+    lock = ProbableGiggle::Lock.new(name: 'already_locked', connection: connection, logger: logger)
+    assert lock.locked?
+  end
+
+  def test_locked_when_unlocked
+    connection = FakeMysqlConnection.new(return_value: nil)
+    lock = ProbableGiggle::Lock.new(name: 'already_locked', connection: connection, logger: logger)
+    refute lock.locked?
+  end
+
+  def test_owned
+    assert lock.owned?
+  end
+
   def test_obtain
-    assert @lock.obtain
+    assert lock.obtain
   end
 
   def test_release
-    assert @lock.release
+    assert lock.release
   end
 end
