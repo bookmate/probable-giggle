a8e0c4881ff2ee61df1f0356f91e4490d5b171ee
Nikita Misharin <misharinn@gmail.com>
2017-05-16 17:44:59 +0300

    require secure random

--- lib/probable_giggle/lockable.rb
+++ lib/probable_giggle/lockable.rb
@@ -2,6 +2,8 @@ require 'probable_giggle/lock'
 
 module ProbableGiggle
   module Lockable
+    AlreadyLocked = Class.new(StandardError)
+
     def with_lock(name, timeout: Lock::DEFAULT_TIMEOUT)
       lock = Lock.new(name: name)
       if lock.obtain(timeout)
@@ -16,7 +18,7 @@ module ProbableGiggle
     private
 
     def on_already_locked(lock)
-      fail("Could not obtain lock on [#{lock.name}]")
+      fail AlreadyLocked, "Could not obtain lock on [#{lock.name}]"
     end
   end
 end
